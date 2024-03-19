37820200de7b7717d0ae4219c32fcc4405a4b143
wuth <906650474@qq.com>
2024-03-19 10:08:55 +0800

    feat: router

--- src/routes/users.js
+++ src/routes/users.js
@@ -10,4 +10,13 @@ router.get('/bar', function (ctx, next) {
   ctx.body = 'this is a users/bar response'
 })
 
+router.post('/login', async (ctx, next) => {
+  const { username, password} = ctx.request.body;
+  // 登陆验证的逻辑
+  ctx.body = {
+    username,
+    password
+  }
+})
+
 module.exports = router
