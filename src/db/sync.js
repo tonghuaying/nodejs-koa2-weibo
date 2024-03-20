5ce1e1af2d25d134380f60c3ccc200bf359ffccd
wuth <906650474@qq.com>
2024-03-20 10:50:42 +0800

    feat: add eslint

---
+++ src/db/sync.js
@@ -0,0 +1,22 @@
+/**
+ * @description sequelize 同步数据库
+ * @author th
+ */
+
+const seq = require('./seq');
+
+// require('./model');
+
+
+// 测试链接
+seq.authenticate().then(() => {
+  console.log('auth ok')
+}).catch(() => {
+  console.log('auth err')
+})
+
+// 执行同步
+seq.sync({ force: true }).then(() => {
+  console.log('sync ok')
+  process.exit()
+})
