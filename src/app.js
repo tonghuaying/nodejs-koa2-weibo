e0a98322ad2f5c3a17670d927aa120e02bf1b6ce
wuth <906650474@qq.com>
2024-03-21 11:41:33 +0800

    feat: add error and 404 page

--- src/app.js
+++ src/app.js
@@ -6,11 +6,21 @@ const onerror = require('koa-onerror')
 const bodyparser = require('koa-bodyparser')
 const logger = require('koa-logger')
 
+const { REDIS_CONF } = require('./conf/db');
+const { isProd } = require('./utils/env');
+// 路由
 const index = require('./routes/index')
 const users = require('./routes/users')
+const errorViewRouter = require('./routes/view/error')
 
 // error handler
-onerror(app)
+let onerrorConf = {};
+if (isProd) {
+  onerrorConf = {
+      redirect: '/error'
+  }
+}
+onerror(app, onerrorConf)
 
 // middlewares
 app.use(bodyparser({
@@ -35,6 +45,7 @@ app.use(views(__dirname + '/views', {
 // routes
 app.use(index.routes(), index.allowedMethods())
 app.use(users.routes(), users.allowedMethods())
+app.use(errorViewRouter.routes(), errorViewRouter.allowedMethods())
 
 // error-handling
 app.on('error', (err, ctx) => {
