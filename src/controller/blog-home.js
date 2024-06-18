8615cc4b7f6861437b790db71adc4ebda91d2e13
wuth <906650474@qq.com>
2024-06-18 15:24:29 +0800

    feat: 首页

--- src/controller/blog-home.js
+++ src/controller/blog-home.js
@@ -4,9 +4,9 @@
  */
 const xss = require('xss');
 const { SuccessModel, ErrorModel } = require("../model/ResModel");
-const { createBlog } = require("../services/blog");
+const { createBlog, getFollowersBlogList } = require("../services/blog");
 const { createBlogFailInfo } = require("../model/ErrorInfo");
-
+const { PAGE_SIZE, REG_FOR_AT_WHO } = require('../conf/constant')
 /**
  * 创建微博
  * @param {Object} param0 创建微博所需数据 { userId, content, image }
@@ -26,6 +26,32 @@ async function create({ userId, content, image }) {
 	}
 }
 
+/**
+ * 获取首页微博列表
+ * @param {number} userId userId
+ * @param {number} pageIndex page index
+ */
+ async function getHomeBlogList(userId, pageIndex = 0) {
+	const result = await getFollowersBlogList(
+			{
+					userId,
+					pageIndex,
+					pageSize: PAGE_SIZE
+			}
+	)
+	const { count, blogList } = result
+
+	// 返回
+	return new SuccessModel({
+			isEmpty: blogList.length === 0,
+			blogList,
+			pageSize: PAGE_SIZE,
+			pageIndex,
+			count
+	})
+}
+
 module.exports = {
 	create,
+	getHomeBlogList
 };
